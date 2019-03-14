64695120cd25aaa94cee40ad263f6864a5708aec
Eleni Nikou <eleni.nikou@chasacademy.se>
2019-03-14 15:01:50 +0200

    milestone controller done

--- app/Http/Controllers/MilestoneController.php
+++ app/Http/Controllers/MilestoneController.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Milestone;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Validator;
+use App\Milestone;
+use App\Project;
 
 class MilestoneController extends Controller
 {
@@ -25,7 +27,30 @@ class MilestoneController extends Controller
     // Create milestone 
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'focus' => 'required',
+            'project_id' => 'required',
+            'due_date' => 'required',
+        ]);
+            
+        if ($validator->fails()) {
+            return response()->json(['error'=>$validator->errors()], 401); 
+
+        } else {
+
+            $milestone = [
+                'title' => $request->title,
+                'focus' => $request->focus,
+                'project_id' => $request->project_id,
+                'due_date' => $request->due_date,
+            ];
+                
+            Milestone::create($milestone);
+            return response()->json(['milestone' => $milestone, 'message' => 'Milestone was created']);
+        }
+        return response()->json(['message' => 'Could not create milestone']);
+
     }
 
 
@@ -38,14 +63,49 @@ class MilestoneController extends Controller
 
 
     // Edit milestone
-    public function update(Request $request, Milestone $milestone)
+    public function update(Request $request, $id)
     {
-        //
+        $milestone = Milestone::find($id);
+        $project = Project::find($milestone->project_id);
+
+
+        if ($request->user_id == $project->creator_id) {
+
+            $validator = Validator::make($request->all(), [
+                'title' => 'required',
+                'focus' => 'required',
+                'project_id' => 'required',
+                'due_date' => 'required',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['error'=>$validator->errors()], 401); 
+            } else {
+                $milestone->title = $request->get('title');
+                $milestone->focus = $request->get('focus');
+                $milestone->project_id = $request->get('project_id');
+                $milestone->due_date = $request->get('due_date');
+                $milestone->save();
+                return response()->json(['milestone' => $milestone, 'message' => 'Milestone was updated']);
+            }
+        } else {
+            return response()->json(['message' => 'You can only update milestones in projects that you have created']);
+        }
     }
 
     // Delete milestone
-    public function destroy(Milestone $milestone)
+    public function destroy($id)
     {
-        //
+        $user = Auth::user();
+        $milestone = Milestone::find($id);
+        $project = Project::find($milestone->project_id);
+
+        if ($user->id == $project->creator_id) {
+            $milestone->tickets()->delete();    
+            $milestone->delete();
+            return response()->json(['message' => 'Milestone was deleted']);
+        } else {
+            return response()->json(['message' => 'You can only delete milestones in projects that you have created']);
+        }
     }
 }
