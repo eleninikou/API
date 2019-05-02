07540e696b0bbc7b82cddcdc1fa3e463a4eb33ae
Eleni Nikou <eleni.nikou@chasacademy.se>
2019-05-02 15:54:25 +0300

    mail blade update

--- resources/views/emails/invite.blade.php
+++ resources/views/emails/invite.blade.php
@@ -4,7 +4,7 @@
 ## Just click on the button below and follow the steps to start working on the project: {{ $invitation->project_name }}!
 
 
-@component('mail::button', ['url' => '{{ env('APP.URL') }}/accept/'.$invitation->token , 'color' => 'success'])
+@component('mail::button', ['url' => '{{ env(APP_URL) }}/accept/'.$invitation->token , 'color' => 'success'])
     Accept invitation
 @endcomponent
 
