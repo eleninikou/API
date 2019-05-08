afc8e79b31d5b698cdae4935360aea5815fe3347
Eleni Nikou <eleni.nikou@chasacademy.se>
2019-05-08 10:40:03 +0300

    mail blade

--- resources/views/emails/invite.blade.php
+++ resources/views/emails/invite.blade.php
@@ -1,13 +1,13 @@
 @component('mail::message')
 
-# Hi, You are invited to join our team!
-## Just click on the button below and follow the steps to start working on the project: {{ $invitation->project_name }}!
+# You were just invited to join our team!
+## Click on the link below and follow the steps to start working with us on the project: 
+# {{ $invitation->project_name }}!
 
 
 @component('mail::button', ['url' => 'https://itsm-frontend.herokuapp.com/accept/'.$invitation->token , 'color' => 'success'])
-    Accept invitation
+    Join {{ $invitation->project_name }}
 @endcomponent
 
-[image]: {{asset('/environment.png')}} "Image"
 
 @endcomponent
