4efca9490f30c4f874e48114305a4b4f36ea83ae
Eleni Nikou <eleni.nikou@chasacademy.se>
2019-05-13 19:19:35 +0200

    register errormessages bugfix

--- app/Http/Controllers/API/PassportController.php
+++ app/Http/Controllers/API/PassportController.php
@@ -52,7 +52,7 @@ class PassportController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'required|max:255',
             'email' => 'required|string|max:255',
-            'password' => 'required|string|min:6',
+            'password' => 'required|string',
             ]);
             
             if ($validator->fails()) {
